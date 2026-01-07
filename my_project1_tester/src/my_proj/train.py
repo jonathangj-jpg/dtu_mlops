4fb113b1f3bd506c3df595307e881aca433209fb
jonathangj-jpg <s245465@dtu.dk>
2026-01-07 10:25:22 +0100

    ændringer i week 1 revoked

---
+++ my_project1_tester/src/my_proj/train.py
@@ -0,0 +1,10 @@
+from my_proj.model import Model
+from my_proj.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
